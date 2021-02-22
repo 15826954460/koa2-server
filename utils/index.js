7dd894916cfa7ce3d899b4540e1257b868092cad
15826954460 <624793604@qq.com>
2021-02-22 21:51:47 +0800

    feat: 添加全局工具方法

---
+++ utils/index.js
@@ -0,0 +1,12 @@
+/**
+ * @author 柏运送
+ * @date 2021-02-22 21:51:07
+ * @description 全局工具函数
+*/
+
+module.exports = {
+  judgeType: (val) => {
+    const str = {}.prototype.toString(val);
+    return str.slice(8, str.length -1).toLowerCase();
+  }
+};

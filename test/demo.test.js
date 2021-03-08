030279471b350e8a12e76db51678522844658b3d
15826954460 <624793604@qq.com>
2021-03-08 22:55:53 +0800

    feat:单元测试重构

--- test/demo.test.js
+++ test/demo.test.js
@@ -5,6 +5,9 @@
 */
 
 const assert = require('assert');
+const server = require('./server');
+
+// 判断数据用列
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the [1, 2, 3] is not have 4', function() {
@@ -17,10 +20,28 @@ function sum(a, b) {
   return a + b;
 }
 
+// 求和用列
 describe('求和', function() {
   describe('sum', function() {
     it('1 add 3 should return 4', () => {
       assert.equal(sum(1, 3), 4);
     });
+  });
+});
+
+// 接口测试用列
+describe('http get json', () => {
+  it(`get json 返回数据格式正确`, async () => {
+    const getRes = await server.get('/json'); // get 请求
+    // const posRes = await server.post('/login').send({
+    //   userName: 'xxxx',
+    //   password: 123,
+    // }); // post 请求
+    assert.deepStrictEqual(getRes.body, { title: 'koa2 json' });
+  });
+
+  it(`get string 返回数据类型正确`, async () => {
+    const getRes = await server.get('/string'); // get 请求
+    assert.ok(getRes.body, 'koa2 string');
   });
 });
